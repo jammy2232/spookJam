79323d32007c1adb05c0349368d0b4b3a987e452
JAMES MCADAM <0702207@uad.ac.uk>
2018-11-03 12:39:01 +0000

    Added a sprite render with billboard to allow for 2d in 3d world

---
+++ spookJam/Assets/Script/RenderComponent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderComponent : MonoBehaviour
+{
+
+    public Sprite spriteToRender = null;
+    public SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void OnEnable ()
+    {
+
+        if(!spriteToRender)
+        {
+            Debug.Log(gameObject.name + " has no sprite assigned on the rendercomponent.");
+            return;
+        }
+
+        // Find reference the the Billboard
+        Transform[] objects = GetComponentsInChildren<Transform>();
+
+        foreach(Transform trans in objects)
+        {
+            if(trans.gameObject.name == "BillBoard")
+            {
+                // apply the msprite
+                spriteRenderer = trans.gameObject.GetComponent<SpriteRenderer>();
+                spriteRenderer.sprite = spriteToRender;
+            }
+        }
+
+        if(spriteRenderer == null)
+        {
+            Debug.Log(gameObject.name + " has no Billboard Prefab attached to render it.");
+        }
+		
+	}
+
+}
---
+++ spookJam/Assets/Script/billBoardRendering.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billBoardRendering : MonoBehaviour {
+
+    // 
+    Camera cameraRef;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        // Store the reference to the camera 
+        cameraRef = Camera.main;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // Make the bill board face the camera
+        transform.LookAt(cameraRef.transform);
+        transform.Rotate(transform.up, Mathf.Deg2Rad * 180.0f);
+
+	}
+}
